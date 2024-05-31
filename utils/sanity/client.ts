dc8210ff4582aa703b3d7a464c4168e90806abf8
Marcus <marcus@thinkingbox.com>
2024-05-31 13:39:50 -0400

    [#0] - Link to the new project

--- utils/sanity/client.ts
+++ utils/sanity/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@sanity/client"
 
 export const client = createClient({
-   projectId: 'gf6pa0yr', 
+   projectId: 'm9kr2eer', 
    dataset: "production", 
    apiVersion: "2024-03-11",
    useCdn: true, 
