9712b989bf528b4c7f8ca56c45815b4b9e4c7ac4
Marcus <marcus@thinkingbox.com>
2024-06-25 15:32:47 -0400

    [#0] - Set up preview

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   sanity: {
     globalHelper: true,
     projectId: 'l202cs32',
+    token: 'skn2H30YylmoJaE83LBlmdQtaF8sbCGLFpX7ILFhbAR4zhQ1Lw2eGAvDpiQIvIy2gUPkuPk51rK6pprMYqSkD5Fznfe1cnRhvHNJRgI0TvWlHgX8f8JIQRax3rY5la4JZvRGALM6TVCOeZXSSwx7XWTihVBXp4dJIiA45FlmSwUSOOcD4WXu',
     dataset: 'production',
     useCdn: true,
     ignoreBrowserTokenWarning: true,
